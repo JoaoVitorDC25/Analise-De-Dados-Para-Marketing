1410cdb6d990b362efff3be656fcbdaae5dc7a7c
João Vitor Dias <joaovitordiasc.25@gmail.com>
2026-07-27 23:34:25 -0300

    feat: add func hist_graphic in charts.py

--- charts.py
+++ charts.py
@@ -4,9 +4,33 @@ import seaborn as sns
 import config as cfg
 
 def hist_graphic(
+    valor_col,
+    media_valor,
+    mediana_valor,
+    std_valor,
+    xLabel,
+    yLabel,
+    titulo=None,
+    figsize=cfg.FIG_SIZE_HIST,
     caminho=None):
     
+    plt.figure(figsize=figsize)
     
+    plt.hist(valor_col, bins = 30, color = 'skyblue', edgecolor = 'black', alpha = 0.7)
+    plt.axvline(media_valor, color = 'red', linestyle='--', linewidth = 2, label= f'Media = R${media_valor:.2f}')
+    plt.axvline(mediana_valor, color = 'orange', linestyle='--', linewidth = 2, label= f'Mediana = R${mediana_valor:.2f}')
+    plt.axvline(media_valor + std_valor, color = 'green', linestyle=':', linewidth = 2, label= f'+1 DP = R${media_valor + std_valor:.2f}')
+    plt.axvline(media_valor - std_valor, color = 'green', linestyle=':', linewidth = 2, label= f'-1 DP = R${media_valor - std_valor:.2f}')
+    
+    plt.title(titulo)
+    
+    plt.xlabel(xLabel)
+    plt.ylabel(yLabel)
+    
+    plt.legend()
+    plt.grid(alpha = cfg.ALPHA_HIST)
+
+    caminho = f"image/{titulo.replace(" ","")}.png"
     plt.tight_layout()
     plt.savefig(caminho, dpi=cfg.DPI, bbox_inches='tight') if caminho else None
     plt.show() 
@@ -24,7 +48,6 @@ def heatmap_graphic(
     plt.title(titulo)
     
     caminho = f"image/{titulo.replace(" ","")}.png"
-    
     plt.tight_layout()
     plt.savefig(caminho, dpi=cfg.DPI, bbox_inches='tight') if caminho else None
     plt.show()
